2cde4a36dc85d8c0bb0bd0b397b3362a04e2a3c2
Peyman Taeb <ptaeb@coconut.dmes.fit.edu>
2018-06-19 10:46:36 -0400

    Forecast Upload

--- R/run_IRLSetup.R
+++ R/run_IRLSetup.R
@@ -1,5 +1,5 @@
 # run_IRLSetup.R
 
-source('R/download_gefs_data.R')
-source('R/download_asos_data.R')
-source('R/calculate_irl_setup.R')
+source('~/wind-setup/IRLSetup/R/download_gefs_data.R')
+source('~/wind-setup/IRLSetup/R/download_asos_data.R')
+source('~/wind-setup/IRLSetup/R/calculate_irl_setup.R')
