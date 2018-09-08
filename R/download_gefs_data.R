4780cc97715522ddc512561d7ca6254258dd88d0
FL-ASGS <ptaeb@coconut.dmes.fit.edu>
2018-09-08 17:54:38 -0400

    Forecast Update

--- R/download_gefs_data.R
+++ R/download_gefs_data.R
@@ -145,8 +145,7 @@ for (ens.mem in ens.mems) {
         # for (fcst.hour in 0:0) { # for testing purposes
         
         # download the file for this ensemble member and forecast hour
-        # options(error = recover)
-        options(error=NULL)
+        options(error = recover)
         gefs.file <- downloadGRIB(get_inv.path, get_grib.path, ens.mem, date, 
                                   run, getFcstHrString(fcst.hour), tmp.path)
         
