29ea2c9875e3a4fdd67e9b1ecc24f10117193740
FL-ASGS <ptaeb@coconut.dmes.fit.edu>
2018-09-13 16:13:28 -0400

    Forecast Update

--- R/download_gefs_data.R
+++ R/download_gefs_data.R
@@ -142,11 +142,7 @@ for (ens.mem in ens.mems) {
     # loop through all forecast hours and download data
     for (fcst.hour in seq(0, 129, by = 3)) {
         # Dumb download for avoiding getting stuck
-        url <- paste('http://www.ftp.ncep.noaa.gov/data/nccf/com/gens/prod/gefs.',
-                   date, '/', run, '/pgrb2ap5/', ens.mem, '.t', run,
-                   'z.pgrb2a.0p50.f', fcst.hour)
-
-        wget(url)
+        wget(getGRIBurl(ens.mem, date, run, fcst.hour[]))
         
         gefs.file <- downloadGRIB(get_inv.path, get_grib.path, ens.mem, date, 
                                   run, getFcstHrString(fcst.hour), tmp.path)
